57f758f0f9fb55f97eb34a08b2de1c50328d00e9
salman-abedin <salman104alvi@gmail.com>
2021-06-21 11:02:14 +0600

    added a preloader

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,20 +5,27 @@ add_class_on_focus({
 
 // toggle_navigation_on_scroll()
 
-doms('#form_sign_up')[0].onsubmit = async (event) => {
-  event.preventDefault()
-  window.location.href = '/register';
-  // const { full_name, email } = Object.fromEntries(new FormData(event.target))
-  // console.log(full_name, email)
-  // try{
-  //   const result = await auth.createUserWithEmailAndPassword(email, 'asdflkj')
-  //   console.log(result)
-  // }catch(error){
-  //   console.log(error)
-  // }
+// doms('#form_sign_up')[0].onsubmit = async (event) => {
+//   event.preventDefault()
+//   window.location.href = '/register';
+//   // const { full_name, email } = Object.fromEntries(new FormData(event.target))
+//   // console.log(full_name, email)
+//   // try{
+//   //   const result = await auth.createUserWithEmailAndPassword(email, 'asdflkj')
+//   //   console.log(result)
+//   // }catch(error){
+//   //   console.log(error)
+//   // }
+// }
 
-}
+// doms('#sign_in_google')[0].onclick = () => {
+//   sign_in_google()
+// }
 
-doms('#sign_in_google')[0].onclick = () => {
-  sign_in_google()
+window.onload = () => {
+  const preloader = doms('#preloader')[0]
+  preloader.style.opacity = '0'
+  setTimeout(() => {
+    preloader.remove()
+  }, 2000);
 }
