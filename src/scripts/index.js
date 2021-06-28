6e3b32f58367891e64ce73fb05376aa33d6192f8
salman-abedin <salman104alvi@gmail.com>
2021-06-28 10:30:05 +0600

    added button animation

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -38,6 +38,7 @@ try {
 
 try {
   doms('#form_register_club')[0].onsubmit = async (event) => {
+
     event.preventDefault()
 
     const {
@@ -111,27 +112,26 @@ try {
     const gender = doms('#form_register_member #gender')[0].value
     const education = doms('#form_register_member #education')[0].value
 
-    await db.collection('members').doc(id).update({
-      name_first,
-      name_last,
-      email,
-      date_of_birth,
-      profession,
-      organization,
-      contact,
-      position,
-      profile_completed: true,
-      gender,
-      education
-    })
+    // await db.collection('members').doc(id).update({
+    //   name_first,
+    //   name_last,
+    //   email,
+    //   date_of_birth,
+    //   profession,
+    //   organization,
+    //   contact,
+    //   position,
+    //   profile_completed: true,
+    //   gender,
+    //   education
+    // })
 
-    console.log('done')
     doms('#confirm')[0].style.display = 'block'
-    // window.location.href = '/';
-
+    await sleep(3000)
+    window.location.href = '/';
   }
 } catch (error) {
-  //
+  console.log(error)
 }
 
     // const club_members_docs = await db.collection('members').where('club_id', '==', 'zrHIFHBJHvSz3V5jFjeh').get()
