2a61c5051080bbac5f9aab01fe13510193c09de8
salman-abedin <salman104alvi@gmail.com>
2021-07-12 23:55:11 +0600

    stopped using webps
    dev

---
+++ snowpack.config.mjs
@@ -0,0 +1,54 @@
+import imagemin from 'imagemin';
+import imageminJpegtran from 'imagemin-jpegtran';
+import imageminPngquant from 'imagemin-pngquant';
+
+const files = await imagemin(['public/images/*.{jpg,png}'], {
+  destination: 'public/images/optimized',
+  plugins: [
+    imageminJpegtran(),
+    imageminPngquant({
+      quality: [0.6, 0.8]
+    })
+  ]
+});
+
+export default {
+  mount: {
+    public: { url: "/public", static: true, resolve: false },
+    src: "/",
+  },
+  exclude: [
+    // '**/src/styles/**',
+    // '**/src/markups/**',
+  ],
+  plugins: [
+    ["@marlonmarcello/snowpack-plugin-pug",
+      {
+        data: {
+        }
+      }
+    ],
+    [
+      '@snowpack/plugin-sass',
+      {
+        compilerOptions: {
+          style: "compressed",
+        },
+      },
+    ],
+    '@snowpack/plugin-webpack',
+    "@snowpack/plugin-babel",
+    '@snowpack/plugin-postcss',
+  ],
+
+  devOptions: {
+    port: 1234,
+    bundle: false,
+    out: "dist",
+    open: "none"
+  },
+  buildOptions: {
+    baseUrl: './',
+    out: "dist"
+  },
+};
