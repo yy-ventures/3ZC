08aef744d6ac77b84714d39ea878348354cc2e85
salman-abedin <salman104alvi@gmail.com>
2021-07-12 16:57:19 +0600

    removed css nano

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,11 @@
+// const cssnano = require('cssnano');
 const postcss_preset_env = require('postcss-preset-env');
-const cssnano = require('cssnano');
 
 module.exports = {
   plugins: [
-    postcss_preset_env(),
-    cssnano(),
+    // cssnano(),
+    postcss_preset_env({
+      autoprefixer: {},
+    }),
   ],
 };
