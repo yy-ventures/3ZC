db8e2908bef14569ddd4919737ca35c349302de7
salman-abedin <salman104alvi@gmail.com>
2021-07-13 03:54:45 +0600

    added polyfill

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,19 @@ module.exports = function(api) {
 
   api.cache(true);
 
-  const presets = ["@babel/preset-env"];
+  const presets = [
+    [
+      "@babel/preset-env",
+      {
+        useBuiltIns: "usage",
+        corejs: 3,
+        targets: {
+          browsers: ["last 2 Chrome versions"]
+        }
+      }
+    ]
+  ];
+
   const plugins = [];
 
   return { presets, plugins };
